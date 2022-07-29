bdeee6962cd9c06fab95f8774e6864f5ab05ae76
Nilmar Esterci <esterci67@hotmail.com>
2022-07-28 23:02:15 -0300

    new

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
+
 //Rotas
 const index = require('./routes/index');
-const defaultRoute = require('./routes/defaultRoute');
+const personRoute = require('./routes/personRoute');
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use('/', index);
-app.use('/persons', defaultRoute);
+app.use('/person', personRoute);
+
 module.exports = app;
