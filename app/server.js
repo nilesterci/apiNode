269ec879c8198b09ef9cec7d3b645206fb7eed6a
Nilmar Esterci <esterci67@hotmail.com>
2022-07-28 23:41:28 -0300

    new

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('./src/Routes/index')(app);
+require('../src/Routes/index')(app);
 app.use(cors());
 app.use(express.json());
 app.listen(process.env.PORT || 3000, function(){
