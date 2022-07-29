e0104b31a8f7649f64671660ae2643a762c600d7
Nilmar Esterci <esterci67@hotmail.com>
2022-07-29 00:06:18 -0300

    new

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 require('./src/Routes/index')(app);
 app.use(cors());
 app.use(express.json());
-app.listen(process.env.PORT || 5000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
